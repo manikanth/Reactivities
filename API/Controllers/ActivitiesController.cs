5f0367e484f1ac2c56b0ea45c5bda6b7b5d6c6de
Manikanth Rallapalli <manikanthr@ymail.com>
2021-07-08 07:15:18 +0530

    end of module 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Persistence;
 using MediatR;
 using Application.Activities;
+using System.Threading;
 
 namespace API.Controllers
 {
@@ -23,7 +24,24 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query{Id = id});
         }
-    }
 
-    
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+    }
 }
